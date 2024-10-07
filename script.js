e3c585bc1d64b9b7ce708c0f0fdb5e25898ce94f
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-10-07 07:29:26 +0000

    markup complete

---
+++ script.js
@@ -0,0 +1,36 @@
+const endDate = "1 January 2025 00:00 AM";
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+// const clock = () => {
+
+// }
+
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+
+  // countdown over
+  if (diff < 0) return;
+
+  // convert into days
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
+}
+
+clock();
+
+/**
+ * 1 day = 24 hrs
+ * 1 hr = 60 mins
+ * 60 mins = 3600 secs
+ */
+
+// function call in every sec
+setInterval(() => {
+  clock();
+});
