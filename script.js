8691da9c600f88041f740c2ebc1a9ee3513d28f6
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-10-07 07:33:13 +0000

    add math.foor function

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function clock() {
   // countdown over
   if (diff < 0) return;
 
-  // convert into days
+  // convert into days 
   inputs[0].value = Math.floor(diff / 3600 / 24);
   inputs[1].value = Math.floor(diff / 3600) % 24;
   inputs[2].value = Math.floor(diff / 60) % 60;
