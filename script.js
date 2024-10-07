1657433341c019fd5d9229c7e0044c85a4bfac0e
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-10-07 07:36:01 +0000

    set endDate

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const endDate = "1 January 2025 00:00 AM"; 
+const endDate = "1 January 2025 00:00 AM";
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input");
